c6a0f56ebec25579980a5e97dae4bb23adf6eb83
Chris Owen <cowen1992@hotmail.co.uk>
2020-06-07 12:43:40 +0100

    Create Node Challenge Repo

---
+++ server.js
@@ -0,0 +1,34 @@
+// server.js
+// This is where your node app starts
+
+//load the 'express' module which makes writing webservers easy
+const express = require("express");
+const app = express();
+
+//load the quotes JSON
+const quotes = require("./quotes.json");
+
+// Now register handlers for some routes:
+//   /                  - Return some helpful welcome info (text)
+//   /quotes            - Should return all quotes (json)
+//   /quotes/random     - Should return ONE quote (json)
+app.get("/", function (request, response) {
+  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+});
+
+//START OF YOUR CODE...
+
+//...END OF YOUR CODE
+
+//You can use this function to pick one element at random from a given array
+//example: pickFromArray([1,2,3,4]), or
+//example: pickFromArray(myContactsArray)
+//
+function pickFromArray(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+//Start our server so that it listens for HTTP requests!
+const listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
