1dbdb14551d70b2c8a998ef8f8ace85f173b5a99
mahmutsel <mahmutsel29@gmail.com>
2020-06-24 16:17:53 +0100

    Added /quotes successfully

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.get("/", function (request, response) {
 
 //START OF YOUR CODE...
 
+app.get("/quotes", (req, res) => {
+  res.send(quotes);
+});
+
+
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
