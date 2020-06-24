9f3953af1f3997215622cd54551bc0bfaa57be36
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-06-24 21:46:08 +0100

    add cors

--- server.js
+++ server.js
@@ -3,7 +3,11 @@
 
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
+const cors = require('cors')
+
 const app = express();
+app.use(cors())
+
 
 //load the quotes JSON
 const quotes = require("./quotes-with-id.json");
