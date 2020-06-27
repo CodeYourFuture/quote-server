67231b1a1ca1a3188d5236eda9abfd9864b593cc
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-06-27 23:18:53 +0100

    lodash-downloaded-exended

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
+const lodash = require("lodash");
 const app = express();
 
 //load the quotes JSON
@@ -23,7 +24,7 @@ app.get('/quotes',(req, res)=>{
 });
 
 app.get('/quotes/random', (req, res)=>{
-  res.send(pickFromArray(quotes))
+  res.send(lodash.sample(quotes))
 })
 
 app.get('/quotes/search', (req, res)=>{
