5184223e1f623d17f6a872742f623da0b03621c0
hamidreza <hrfarzadeh@gmail.com>
2020-06-27 23:56:58 +0100

    test3

--- server.js
+++ server.js
@@ -53,11 +53,11 @@ function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
- const PORT = process.env.PORT || 38623;
+const PORT = process.env.PORT || 4400;
 
-// app.listen(PORT, () => console.log(`server started on prot ${PORT}`));
+app.listen(PORT, () => console.log(`server started on prot ${PORT}`));
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().PORT);
-});
+// const listener = app.listen(process.env.PORT, function () {
+//   console.log("Your app is listening on port " + listener.address().port);
+// });
