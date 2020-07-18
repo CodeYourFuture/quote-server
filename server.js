308e80e7922c08ccd9c50ffc2d9f7737cd11f615
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-07-18 18:19:02 +0100

    connection with mongodb/add env/

--- server.js
+++ server.js
@@ -4,55 +4,72 @@
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
 const cors = require('cors')
+const mongodb = require('mongodb')
+const dotenv = require("dotenv");
+
+ dotenv.config()
+const url = process.env.MONGOLAB_URI;
+
 
 const app = express();
 app.use(cors())
 
 
 //load the quotes JSON
-const quotes = require("./quotes-with-id.json");
+//const quotes = require("./quotes-with-id.json");
 
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
-app.get("/", function (request, response) {
-  response.send("Min's Quote Server!  Ask me for /quotes/random, or /quotes");
+app.get("/", function (req, res) {
+  const client =  mongodb.MongoClient(url)
+   client.connect(()=>{
+     const db = client.db("quotes")
+     const collection = db.collection("quotes")
+
+     collection.find().toArray((err,quotes)=>{
+       res.sent(err||quotes)
+       client.close()
+     })
+
+   })
+  res.send("Min's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
 //START OF YOUR CODE...
-app.get("/quotes/random", (req,res)=>{
-  res.send(pickFromArray(quotes))
-})
+// app.get("/quotes/random", (req,res)=>{
+//   res.send(pickFromArray(quotes))
+// })
 
-app.get("/quotes",(req,res)=>{
-  res.json(quotes)
-})
+// app.get("/quotes",(req,res)=>{
+//   res.json(quotes)
+// })
 
-app.get("/quotes/search", (req,res)=>{
-  console.log(`We are searching for ${req.query.term}`);
-  if(req.query.term){
-    const searchQuote = quotes.filter(
-      (quote) =>
-        quote.quote.toLowerCase().includes(req.query.term.toLowerCase()) ||
-        quote.author.toLowerCase().includes(req.query.term.toLowerCase())
-    );
-    res.send(searchQuote)
-  }
+// app.get("/quotes/search", (req,res)=>{
+//   console.log(`We are searching for ${req.query.term}`);
+//   if(req.query.term){
+//     const searchQuote = quotes.filter(
+//       (quote) =>
+//         quote.quote.toLowerCase().includes(req.query.term.toLowerCase()) ||
+//         quote.author.toLowerCase().includes(req.query.term.toLowerCase())
+//     );
+//     res.send(searchQuote)
+//   }
   
-})
-app.get("/quotes/minko",(req,res)=>{
-  res.json(quotes)
-})
-//...END OF YOUR CODE
+// })
+// app.get("/quotes/minko",(req,res)=>{
+//   res.json(quotes)
+// })
+// //...END OF YOUR CODE
 
-//You can use this function to pick one element at random from a given array
-//example: pickFromArray([1,2,3,4]), or
-//example: pickFromArray(myContactsArray)
-//
-function pickFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
+// //You can use this function to pick one element at random from a given array
+// //example: pickFromArray([1,2,3,4]), or
+// //example: pickFromArray(myContactsArray)
+// //
+// function pickFromArray(arr) {
+//   return arr[Math.floor(Math.random() * arr.length)];
+// }
 
 //Start our server so that it listens for HTTP requests!
 let port = process.env.PORT;
