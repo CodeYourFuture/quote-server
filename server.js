afce362a856756b5390e3a7ae17f5dabd78b9b7c
Buchraatkeh1984 <61603626+Buchraatkeh1984@users.noreply.github.com>
2020-08-12 12:17:25 +0100

    Buchra quote server Homework

--- server.js
+++ server.js
@@ -7,17 +7,37 @@ const app = express();
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
-
+const lodash = require('lodash.sample');
+let cors = require('cors');
+app.use(cors());
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
-app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+app.get("/", function(request, response) {
+  response.send("Buchra's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
 //START OF YOUR CODE...
 
+app.get("/quotes", function(request, response) {
+  response.send(quotes);
+});
+
+app.get("/quotes/random", function(request, response) {
+  //response.send(pickFromArray(quotes));
+  response.send(lodash(quotes));
+});
+
+app.get("/quotes/search", function(request, response) {
+  let term= request.query.term;
+  let searchedQuotes=quotes;
+  if (term!=undefined){
+    searchedQuotes=quotes.filter
+    (quote=> (quote.quote.toLowerCase().includes(term.toLowerCase()))||(quote.author.toLowerCase().includes(term.toLowerCase()))
+  )}
+  response.send(searchedQuotes);
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
@@ -29,6 +49,6 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
