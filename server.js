13cb2643d6619e027d76ad0907aaa3f03d1e8190
supertata <laetitiachira@gmail.com>
2020-08-13 15:03:27 +0200

    added lodas library

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
+const lodash = require("lodash");
 const app = express();
 
 //load the quotes JSON
@@ -21,9 +22,14 @@ app.get("/quotes", (request, response) => {
   response.send(quotes);
 });
 
+// app.get("/quotes/random", (request, response) => {
+//   response.send(pickFromArray(quotes));
+// });
+
 app.get("/quotes/random", (request, response) => {
-  response.send(pickFromArray(quotes));
+  response.send(_.sample(quotes));
 });
+
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
