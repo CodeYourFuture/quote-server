34896f937eb5fcc12bc41e7d70adb0ac4e1f3f52
zision <zision@gmail.com>
2020-08-19 01:52:04 +0100

    zision-node-week1 hw.

--- server.js
+++ server.js
@@ -13,11 +13,39 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send(
+    "Zision's Quote Server!  Ask me for /quotes/random, or /quotes"
+  );
 });
 
 //START OF YOUR CODE...
+app.get("/quotes", (request, response) => {
+  response.send(quotes);
+});
+app.get("/quotes/random", (request, response) => {
+  response.send(pickFromArray(quotes));
+});
 
+app.get("/quotes/search", (request, response) => {
+  let searchTerm = request.query.term.toLowerCase();
+  let result;
+
+  if (searchTerm) {
+    result = quotes.filter(
+      (term) =>
+        term.quote.toLowerCase().includes(searchTerm) ||
+        term.author.toLowerCase().includes(searchTerm)
+    );
+  } else {
+    response.send("There is no term to search");
+  }
+
+  if (result.length == 0) {
+    response.send("Term not found");
+  }
+
+  response.send(result);
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
