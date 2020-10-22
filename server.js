9ff6d746ba3742014f9eddd5e12420d0253118a7
AliHaider-1 <alihaiderx736@gmail.comnpm>
2020-10-22 22:39:45 +0100

    step 2 completed

--- server.js
+++ server.js
@@ -21,7 +21,10 @@ app.get("/", function (request, response) {
 app.get("/quotes", function (request, response) {
   response.send(quotes);
 });
-
+//quotes/random
+app.get("/quotes/random", function (request, response) {
+  response.send(pickFromArray(quotes));
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
