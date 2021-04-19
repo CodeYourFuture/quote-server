c09dcb2e9b83359faba9e7580eeb2dbd29b33938
PatOn <mahomed2278@gmail.com>
2021-04-19 20:58:06 +0100

    let try

--- server/server.js
+++ server/server.js
@@ -48,6 +48,19 @@ app.get("/api/quotes/random", function (request, response) {
 //   console.log("Your app is listening on port " + listener.address().port);
 // });
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
 const listener = app.listen(PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
