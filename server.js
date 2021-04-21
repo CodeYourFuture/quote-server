b43b49a29a472721f4e6d57c700accb04c86e757
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-04-21 23:38:37 +0100

    quote server created

--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 // This is where your node app starts
 
 //load the 'express' module which makes writing webservers easy
+const { response } = require("express");
 const express = require("express");
 const app = express();
-
+const port = process.env.PORT || 4000;
 //load the quotes JSON
 const quotes = require("./quotes.json");
 
@@ -13,11 +14,28 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("Aaishah's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
 //START OF YOUR CODE...
+app.get('/quotes', (request, response) => {
+  response.json(quotes)
+})
 
+app.get('/quotes/random', (req, res) => {
+  res.json(pickFromArray(quotes))  
+})
+
+app.get('/quotes/searched', (req, res) => {
+  const searchedQuote = req.query.search
+  const randomQuotes = quotes.filter((obj) => {
+    if ((obj.quote.toLowerCase().includes(searchedQuote.toLowerCase())) ||(obj.author.toLowerCase().includes(searchedQuote.toLowerCase())) ){
+      return obj;
+    }
+  })
+  res.json(randomQuotes)
+
+})
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
@@ -29,6 +47,9 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+// const listener = app.listen(process.env.PORT, function () {
+//   console.log("Your app is listening on port " + listener.address().port);
+// });
+app.listen(port, () => {
+  console.log("Your app is listening on port " + port);
+})
