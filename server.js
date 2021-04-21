484785329be986478a99597b9aabae3665208830
Chizim Chinuru <garrohat@yahoo.co.uk>
2021-04-22 00:37:18 +0100

    Completed required tasks

--- server.js
+++ server.js
@@ -4,31 +4,59 @@
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
+const lodash = require("lodash");
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
+
+//Check that port 4040 is not in use otherwise set it to a different port
+const PORT = process.env.PORT || 4040;
+
+//You can use this function to pick one element at random from a given array
+//example: pickFromArray([1,2,3,4]), or
+//example: pickFromArray(myContactsArray)
+const pickFromArray = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
 
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
-app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>Chizim's Quote Server! Ask me for /quotes/random, or /quotes</h1>"
+  );
 });
 
 //START OF YOUR CODE...
 
+// Returns a result of array of all the quotes
+app.get("/quotes", (req, res) => {
+  res.send(quotes);
+});
+
+// Randomly select and display ONE quote (json)
+// app.get("/quotes/random", (req, res) => {
+//   res.send(pickFromArray(quotes));
+// });
+
+app.get("/quotes/random", (req, res) => {
+  res.send(lodash.sample(quotes))
+})
+
+// implement search based on the term entered on the browser
+app.get("/quotes/search", (req, res) => {
+  const filteredQuotes = quotes.filter(
+    (quote) =>
+      quote.quote.toLowerCase().includes(req.query.term.toLowerCase()) ||
+      quote.author.toLowerCase().includes(req.query.term.toLowerCase())
+  );
+
+  res.send(filteredQuotes);
+});
+
 //...END OF YOUR CODE
 
-//You can use this function to pick one element at random from a given array
-//example: pickFromArray([1,2,3,4]), or
-//example: pickFromArray(myContactsArray)
-//
-function pickFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
-
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+app.listen(PORT, () => console.log(`Your app is listening on port ${PORT}`));
