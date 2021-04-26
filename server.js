cf4d6f61b678987ced58e51b8eb3da70d51b9e60
Amanuel <weditedi25@gmail.com>
2021-04-26 15:58:58 +0100

    lodash package installed

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
+// const express = require("express");
+
+// const lodash = require("lodash");
+// const app = express();
+
+// const quotes = require("./quotes.json");
+
+// app.get("/", function (request, response) {
+//   response.send(
+//     "Madiha's Quote Generator!  Ask me for /quotes/random, or /quotes"
+//   );
+// });
+
+// app.get("/quotes", function (request, response) {
+//   response.json(quotes);
+// });
+
+// app.get("/quotes/random", function (request, response) {
+//   response.send(lodash.sample(quotes));
+// });
+
+// //Start our server
+// const listener = app.listen(process.env.PORT || 3000, function () {
+//   console.log("Your app is listening on port " + listener.address().port);
+// });
+
 // server.js
 // This is where your node app starts
 
@@ -10,10 +36,10 @@ const quotes = require("./quotes.json");
 
 app.get("/", function (request, response) {
   response.send(
-    `<h1> Welcome to Amanuel's Quote Server!<h1>  
-    <p> To get all quotes  use: /quotes </p>  
-    <p> To get random quotes  use: /quotes/random</p>  
-    <p> To search any term in the  quotes or author use /quotes/search?term=search-term </p>  
+    `<h1> Welcome to Amanuel's Quote Server!<h1>
+    <p> To get all quotes  use: /quotes </p>
+    <p> To get random quotes  use: /quotes/random</p>
+    <p> To search any term in the  quotes or author use /quotes/search?term=search-term </p>
     `
   );
 });
@@ -26,6 +52,7 @@ app.get("/quotes", function (request, response) {
 app.get("/quotes/random", function (request, response) {
   response.json(pickFromArray(quotes));
 });
+
 //  quotes search route
 app.get("/quotes/search", function (request, response) {
   let term = request.query.term;
