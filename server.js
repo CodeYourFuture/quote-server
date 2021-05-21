e15149d7a15aafef100d9d696f4409e5d668b3ab
Hozan Ali <hozan949@gmail.com>
2021-05-21 04:03:16 +0100

    completed level 1 and 2

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
+const lodash = require('lodash');
+
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
@@ -13,10 +15,23 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+    response.send("Hozan's Quote Server!  Ask me for /quotes/random, /quotes or quotes/search?term='a word'");
 });
 
 //START OF YOUR CODE...
+
+//Level 1
+app.get("/quotes", (request, response) => response.send(quotes));
+
+app.get("/quotes/random", (request, response) => response.send(lodash.sample(quotes)));
+
+//Level 2
+app.get("/quotes/search", (request, response) => {
+
+    let term = request.query.term.toLowerCase();
+    let searchedQuote = quotes.filter(quote => quote.quote.toLowerCase().includes(term) || quote.author.toLowerCase().includes(term))
+    response.send(searchedQuote)
+})
 
 //...END OF YOUR CODE
 
@@ -25,10 +40,10 @@ app.get("/", function (request, response) {
 //example: pickFromArray(myContactsArray)
 //
 function pickFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+    return arr[Math.floor(Math.random() * arr.length)];
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+const listener = app.listen(process.env.PORT || 5000, function () {
+    console.log("Your app is listening on port " + listener.address().port);
 });
