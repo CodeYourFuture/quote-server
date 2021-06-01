be1c36c2026d9d34c37d4f5076677bce8bb3e824
ASLANDENIZ <dkelemence@gmail.com>
2021-06-01 10:14:14 +0100

    updated json file

--- server.js
+++ server.js
@@ -13,20 +13,38 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("Aslan's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
-
-//START OF YOUR CODE...
-
-//...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
 //example: pickFromArray([1,2,3,4]), or
 //example: pickFromArray(myContactsArray)
 //
+
 function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
+
+//START OF YOUR CODE...
+app.get("/quotes", function (request, response) {
+  response.json(quotes);
+});
+
+app.get("/quotes/random", function (request, response) {
+  response.json(pickFromArray(quotes));
+});
+
+app.get("/quotes/search", function (request, response) {
+  const { term: searchTerm } = request.query
+
+  response.json(quotes.filter((element) => {
+    return element.quote.toLowerCase().includes(searchTerm.toLowerCase()) || element.author.toLowerCase().includes(searchTerm.toLowerCase())
+  }));
+});
+//...END OF YOUR CODE
+
+
+
 
 //Start our server so that it listens for HTTP requests!
 const listener = app.listen(process.env.PORT, function () {
