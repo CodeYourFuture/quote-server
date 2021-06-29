4652166cf8e7d35e26469f82109fe39340fe11ee
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-06-29 22:10:04 +0200

    random element from collection

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
+const _ = require("lodash");
 const app = express();
 
 //load the quotes JSON
@@ -25,7 +26,8 @@ app.get('/quotes', (req, res) => {
 });
 
 app.get('/quotes/random', (req, res) => {
-  res.send(pickFromArray(quotes));
+  // res.send(pickFromArray(quotes));
+  res.send(_.sample(quotes));
 });
 
 app.get('/quotes/search', (req, res) => {
