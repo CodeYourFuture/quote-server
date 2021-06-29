3877e4c3b4e5ca0248273bf8181c7ba17d8cabe0
RodrigueNdzana <ndzanarodrigue6@gmail.com>
2021-06-29 14:47:55 +0200

    completed level 1

--- server.js
+++ server.js
@@ -5,19 +5,38 @@
 const express = require("express");
 const app = express();
 
+const PortNumber= process.env.PORT || 3000;
+
 //load the quotes JSON
 const quotes = require("./quotes.json");
+//console.log(quotes);
+
+// in order to use an external file in node you must require it
+const quoteWithIdJson= require('./quotes-with-id.json');
 
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("<h1>Rodrigue's Quote Server!  Ask me for /quotes/random, or /quotes</h1>");
 });
 
 //START OF YOUR CODE...
+// /quotes
+app.get("/quotes", function (requestObject, responseObject) {
+  responseObject.json(quotes);
+});
 
+
+// quotes/random 
+app.get("/quotes/random", function (request, response) {
+  response.send(pickFromArray(quotes));
+});
+
+app.listen(PortNumber, () => {
+  console.log(`The server is listening to port ${PortNumber}`)
+})
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
