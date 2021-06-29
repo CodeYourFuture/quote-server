d0eef144d4af50c3e78d41aac7184e0f2e850920
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-06-30 01:49:16 +0200

    complete fetch from server
    pass quotes json object as props to quote component

---
+++ client/src/Quotes.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import Quote from './Quote';
+
+const Quotes = () => {
+  const URL = "http://localhost:5000/quotes";
+
+  const [state, setState] = useState({
+    status: 'loading',
+    quotes: null,
+    error: null,
+  });
+
+  useEffect(() => {
+    fetch(URL)
+      .then(
+        response => response.json(),
+        () => {
+          return {
+            status: 'loading',
+            quotes: null,
+            error: 'Unable to fetch!',
+          };
+        }
+      )
+      .then(result => {
+        setState({
+          status: 'complete',
+          quotes: result,
+          error: result.error,
+        });
+      });
+  }, []);
+
+  if (state.error) {
+    return (
+      <div>
+        <h1>{state.error}</h1>
+      </div>
+    );
+  } else if (state.status === "loading") {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <div>
+          <Quote quotes={state.quotes} />;
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Quotes
