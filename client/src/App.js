0b2f9ba80746fadb1f708040d1fc235ccf716a9e
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-06-30 00:36:45 +0200

    fetch data

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 
 
 function App() {
+  const URL = "http://localhost:5000/quotes";
+  fetch(URL)
+    .then(res => res.json())
+    .then(data => console.log(data));
   return (
     <div className="App">
       app
