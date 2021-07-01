22d36663e25319da6a4d74079a9f5536a762c625
ALEXANDER <msumba83@gmail.com>
2021-07-01 22:01:25 +0200

    done

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // This is where your node app starts
 
 //load the 'express' module which makes writing webservers easy
+const { response } = require("express");
 const express = require("express");
 const app = express();
 
@@ -14,31 +15,31 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("<h1>Alexander's Quote Server!  Ask me for /quotes/random, or /quotes</h1>");
 });
 
 //START OF YOUR CODE...
 
 // level 1
 app.get("/quotes", function(req, res) {
-   res.send(quotes);
-})
+   response.send(quotes);
+});
 
-app.get("/quotes/random", function(req, res) {
-  res.send(pickFromArray(quotes));
+app.get("/quotes/random", function(request, response) {
+  response.send(pickFromArray(quotes));
 });
 
 // level 2
-app.get("/quotes/search", function(req, res) {
-  let word = req.query.word;
+app.get("/quotes/search", function(request, response) {
+  let word = request.query.word;
   if(word) {
     const searchWord = quotes.filter(word => {
       return (word.quote.toLowerCase().includes(word.toLowerCase()) ||
        word.author.toLowerCase().includes(word.toLowerCase()));
     });
-    res.send(searchWord);
+    response.send(searchWord);
   } else {
-    res.send([]);
+    response.send([]);
   }
 });
 //...END OF YOUR CODE
