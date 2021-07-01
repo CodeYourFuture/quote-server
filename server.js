03cf8bbe68abd187f27480634aa17451d208b001
JarrodBen <jarrodb.247@gmail.com>
2021-07-01 12:53:12 +0200

    Level 1

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ app.get("/", function (request, response) {
 
 //START OF YOUR CODE...
 
+app.get("/quotes",  (req, res) => {
+  res.send(quotes);
+});
+
+app.get("/quotes/random",  (req, res) => {
+  res.send(pickFromArray(quotes));
+});
+
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
