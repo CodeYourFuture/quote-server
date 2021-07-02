d59c318c0c00f70ce66856492a08713b5ea4314a
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2021-07-02 20:40:33 +0200

    installing dependacies
    npm init -y
    npm i express

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ app.get("/", function (request, response) {
 });
 
 //START OF YOUR CODE...
-
+app.get("/quotes", function (request, response) {
+  response.send("pickFromArray");
+})
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
