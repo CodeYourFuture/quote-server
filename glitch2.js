91fc211cfa3b9783387eb2043d14f5e47dd75f6d
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-07-06 14:36:24 +0200

    Add lodash and glitch 2 and 4

---
+++ glitch2.js
@@ -0,0 +1,46 @@
+//This code is from week 2 debugging exercise 2 on Glitch
+const express = require("express");
+const app = express();
+
+const quotes = require("./quotes-with-id.json");
+
+app.get("/", function (request, response) {
+	response.send(
+		"Debugging exercise.  GET for /quotes/search?word=the returns an empty array!  Why?"
+	);
+});
+
+//Displays all quotes
+app.get("/quotes", function (request, response) {
+	response.json(quotes);
+});
+
+//Display quote matching an id
+app.get("/quotes/:id?", function (request, response) {
+	const inputId = request.params.id;
+	if (inputId) {
+		const quote = quotes.filter((res) => res.id == inputId);
+		response.json(quote);
+	}
+});
+
+//search by a term
+// const search = (word) => {
+// 	console.log("This is in the function", word);
+// 	let filteredQuotes = quotes.filter((quote) =>
+// 		quote.quote.toLowerCase().includes(word)
+// 	);
+// 	return filteredQuotes;
+// };
+
+app.get("/quotes/search", function (request, response) {
+	console.log("This is in the route", search);
+	const searchWord = request.query.word;
+	const result = quotes.filter((quote) =>
+		quote.quote.toLocaleLowerCase().includes(searchWord)
+	);
+	response.send("This is word", searchWord);
+});
+
+console.log("App is listening");
+app.listen(3000);
---
+++ glitch4.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const lodash = require("lodash");
+
+const quotes = require("./quotes.json");
+
+app.get("/", function (request, response) {
+	response.send(
+		"Madiha's Quote Generator!  Ask me for /quotes/random, or /quotes"
+	);
+});
+
+app.get("/quotes", function (request, response) {
+	response.send(quotes);
+});
+
+app.get("/quotes/random", function (request, response) {
+	response.send(lodash.sample(quotes));
+});
+
+const listener = app.listen(process.env.PORT);
