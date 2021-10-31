fb1a1c02c3c0951e3690b5398672aa5f788e1fc2
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2021-10-31 01:01:17 +0100

    glitch updated

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const express = require("express");
 const app = express();
 //lodash for array sampling
 const lodash = require("lodash");
+const cors = require('cors');
+app.use(cors());
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
