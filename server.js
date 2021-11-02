6e7eeb4c90429efc653023e78d322bf53c52747d
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2021-11-02 17:34:25 +0000

    Level 1 challenge complete

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // This is where your node app starts
 
 //load the 'express' module which makes writing webservers easy
+const { response } = require("express");
 const express = require("express");
 const app = express();
 
@@ -13,10 +14,18 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("Andy's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
 //START OF YOUR CODE...
+
+app.get("/quotes", (request, response) => {
+  response.send(quotes);
+});
+
+app.get("/quotes/random", (request, response) => {
+  response.send(pickFromArray(quotes));
+});
 
 //...END OF YOUR CODE
 
@@ -29,6 +38,6 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 4000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
