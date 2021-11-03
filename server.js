7368c0a26e1c705116463d62c535253c3b534e32
Tara <81445969+Tara-Q@users.noreply.github.com>
2021-11-03 22:07:11 +0000

    done up to End of Level 1 challenge
    added routes for /quotes and /quotes/random

--- server.js
+++ server.js
@@ -13,10 +13,20 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("Tara's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
+
+
 //START OF YOUR CODE...
+
+app.get("/quotes", function (request, response) {
+  response.send(quotes)
+});
+
+app.get("/quotes/random", function (request, response) {
+  response.send(pickFromArray(quotes))
+});
 
 //...END OF YOUR CODE
 
@@ -29,6 +39,6 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(4000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
