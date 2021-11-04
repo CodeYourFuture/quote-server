7c182e9f44b8af7d8eef418041526502e5310100
Dharma <dharmaguadeloupe@gmail.com>
2021-11-04 19:29:19 +0000

    Homework level 1 & 2 completed

--- server.js
+++ server.js
@@ -3,7 +3,9 @@
 
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
@@ -12,11 +14,33 @@ const quotes = require("./quotes.json");
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
-app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+app.get("/", function (req, res) {
+  res.send("Dharma's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
 //START OF YOUR CODE...
+app.get("/quotes", (req, res) => {
+  res.json(quotes);
+});
+
+app.get("/quotes/random", (req, res) => {
+  const pickFromArray = (arr) => arr[Math.floor(Math.random() * arr.length)];
+  res.send(pickFromArray(quotes));
+});
+
+app.get("/quotes/search", (req, res) => {
+  const searchQuotes = (searchQuery) => {
+    if (!searchQuery) {
+      return [];
+    }
+    return quotes.filter(
+      (term) =>
+        term.quote.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        term.author.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  };
+  res.send(searchQuotes(req.query.term));
+});
 
 //...END OF YOUR CODE
 
@@ -24,11 +48,9 @@ app.get("/", function (request, response) {
 //example: pickFromArray([1,2,3,4]), or
 //example: pickFromArray(myContactsArray)
 //
-function pickFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
+//const pickFromArray = arr => arr[Math.floor(Math.random() * arr.length)];
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
