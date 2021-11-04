5901da70ddde52e17e487eae01250b762cf8e1d7
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-11-04 14:24:46 +0000

    update level 1

--- server.js
+++ server.js
@@ -14,10 +14,15 @@ const quotes = require("./quotes.json");
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
   response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
-});
+}); 
 
 //START OF YOUR CODE...
-
+app.get('/quotes', (req, res) => {
+  res.send(quotes);
+})
+app.get("/quotes/random", (req, res) => {
+  res.send(pickFromArray(quotes));
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
@@ -31,4 +36,5 @@ function pickFromArray(arr) {
 //Start our server so that it listens for HTTP requests!
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
+  //console.log("quotes", quotes);
 });
