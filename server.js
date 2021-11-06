42ed504ec4fc0a60d056864e39344895de68f752
Mor Khojasteh <mori.kh1987@gmail.com>
2021-11-06 14:47:06 +0000

    lesson 1 done!

--- server.js
+++ server.js
@@ -12,13 +12,27 @@ const quotes = require("./quotes.json");
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
-app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+app.get("/", function (req, res) {
+  res.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
-//START OF YOUR CODE...
 
+
+//START OF YOUR CODE...
+app.get("/quotes", function (req, res) {
+  res.send(quotes);
+});
+
+app.get("/quotes/random", function (req, res) {
+  res.send(pickFromArray(quotes));
+});
 //...END OF YOUR CODE
+app.get("/quotes/search", function (req, res) {
+  let search = (req.query.term).toLowerCase();
+  const filterQuotes = quotes.filter((quote) => quote.quote.toLowerCase().includes(search) || quote.author.toLowerCase().includes(search))
+  res.send(filterQuotes);
+});
+
 
 //You can use this function to pick one element at random from a given array
 //example: pickFromArray([1,2,3,4]), or
@@ -29,6 +43,6 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 8000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
