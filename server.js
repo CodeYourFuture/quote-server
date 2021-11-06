4ce797d5ea0fa4b88a6dad4e86c5187ee91c0c06
humailhasankhan <h.hasan.khan88@hotmail.com>
2021-11-06 00:54:31 +0000

    completed all tasks for this assignment

--- server.js
+++ server.js
@@ -1,34 +1,44 @@
-// server.js
-// This is where your node app starts
-
-//load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
+const lodash = require("lodash");
+const cors = require("cors");
 
-//load the quotes JSON
+app.use(cors());
+
 const quotes = require("./quotes.json");
 
-// Now register handlers for some routes:
-//   /                  - Return some helpful welcome info (text)
-//   /quotes            - Should return all quotes (json)
-//   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send(
+    "Humail's Quote Server!  Ask me for /quotes/random, or /quotes"
+  );
 });
 
-//START OF YOUR CODE...
+app.get("/quotes", function (request, response) {
+  response.send(quotes);
+});
 
-//...END OF YOUR CODE
+app.get("/quotes/random", function (request, response) {
+  response.send(lodash.sample(quotes));
+});
 
-//You can use this function to pick one element at random from a given array
-//example: pickFromArray([1,2,3,4]), or
-//example: pickFromArray(myContactsArray)
-//
+app.get("/quotes/search", function (request, response) {
+  let searchTerm = request.query.term.toLowerCase();
+  response.send(
+    quotes.filter((element) => {
+      if (
+        element.quote.toLowerCase().includes(searchTerm) ||
+        element.author.toLowerCase().includes(searchTerm)
+      ) {
+        return element;
+      }
+    })
+  );
+});
+
 function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-//Start our server so that it listens for HTTP requests!
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
