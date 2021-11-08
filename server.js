a70e1b270be8c6317dba20fb8748b5ec6dadf09e
aalassv <a.alas.sv@gmail.com>
2021-11-08 17:34:40 +0000

    Level 1 done - adding 3 routes

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
 // server.js
 // This is where your node app starts
-//**********To Start the server**********/
-//**********npm run dev*********//
-///////change this in the package.json/////
-/*  "scripts": {
-    "start": "node server.js",
-    "dev": "nodemon server.js"
-*/
-
-////installing nodemon service to restart the node server in every change.
-////npm install nodemon
 
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
-const cors = require("cors");
 const app = express();
+//declaring cors to restart the server at every change in the file for dev purposes.
+const cors = require("cors");
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
-
+const { request, response } = require("express");
+//call app to use cors
 app.use(cors());
-
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
@@ -34,7 +25,9 @@ app.get("/quotes/random", function (request, response) {
 });
 
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send(
+    "Israel's Quote Server!  Ask me for /quotes/random, or /quotes"
+  );
 });
 
 //START OF YOUR CODE...
@@ -43,12 +36,13 @@ app.get("/", function (request, response) {
 
 //You can use this function to pick one element at random from a given array
 //example: pickFromArray([1,2,3,4]), or
-//example: pickFpromise
+//example: pickFromArray(myContactsArray)
+//
 function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(4000, function () {
+const listener = app.listen(process.env.PORT || 4000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
