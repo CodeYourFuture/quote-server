95b1d491623932d8badd50f700a0848b29b18ff5
Maria Chira <chiramariageorgiana@gmail.com>
2021-12-12 19:05:57 +0000

    Level 1 completed

--- server.js
+++ server.js
@@ -9,15 +9,21 @@ const app = express();
 const quotes = require("./quotes.json");
 
 // Now register handlers for some routes:
+//START OF YOUR CODE...
 //   /                  - Return some helpful welcome info (text)
+app.get("/", (req, res) => {
+  res.send("Welcome to our server!")
+})
+
 //   /quotes            - Should return all quotes (json)
-//   /quotes/random     - Should return ONE quote (json)
-app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+app.get("/quotes", (req, res) => {
+  res.json(quotes);
 });
 
-//START OF YOUR CODE...
-
+//   /quotes/random     - Should return ONE quote (json)
+app.get("/quotes/random", (req, res) => {
+  res.json(pickFromArray(quotes));
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
