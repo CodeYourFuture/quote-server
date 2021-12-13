eda102839d35ac4f4e17b7bbaad93b1577a83c05
Halla-S <86550752+Halla-S@users.noreply.github.com>
2021-12-13 19:00:32 +0000

    level 1 finished

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-// server.js
-// This is where your node app starts
-
-//load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
 
-// Now register handlers for some routes:
-//   /                  - Return some helpful welcome info (text)
-//   /quotes            - Should return all quotes (json)
-//   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send(
+    "<h1>Welcome to the quote server , Ask me for /quotes/random, or /quotes...!</h1>"
+  );
+});
+app.get("/quotes", function (request, response) {
+  response.json(
+    quotes.map((item) => {
+      return `${item.quote}  Author: ${item.author}`;
+    })
+  );
 });
 
-//START OF YOUR CODE...
-
-//...END OF YOUR CODE
-
-//You can use this function to pick one element at random from a given array
-//example: pickFromArray([1,2,3,4]), or
-//example: pickFromArray(myContactsArray)
-//
+app.get("/quotes/random", function (request, response) {
+  function pickFromArray(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+  response.json(pickFromArray(quotes));
+  console.log(`${pickFromArray(quotes)}`);
+});
 function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
