91b80e6353dbb47734fc666020add0588b57e938
Katie <professionalkateg@gmail.com>
2021-12-14 13:58:10 +0000

    completed the basic challanges, will move onto the frontend react part soon

--- server.js
+++ server.js
@@ -4,31 +4,46 @@
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
+const lodash = require("lodash");
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
 
-// Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
-app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+
+app.get("/", (req, res) => {
+  res.send(
+    "<div><h1>Hey! How to use this API: </h1><ul><li>Use /quotes to get a full list of quotes</li><li>/quotes/random to get a random quote.</li><li>/quotes/search?term=:query to search for quotes</li></ul></div>"
+  );
 });
 
-//START OF YOUR CODE...
+// return all quotes
+app.get("/quotes", (req, res) => {
+  res.json(quotes);
+});
 
-//...END OF YOUR CODE
+// return a random quote
+app.get("/quotes/random", (req, res) => {
+  res.json(lodash.sample(quotes));
+});
 
-//You can use this function to pick one element at random from a given array
-//example: pickFromArray([1,2,3,4]), or
-//example: pickFromArray(myContactsArray)
-//
-function pickFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
+app.get("/quotes/search", (req, res) => {
+  const search = req.query.term;
+  if (req.query.term === undefined) {
+    return res.json({
+      msg: "Please specify a search term. /quotes/search?term=:query",
+    });
+  }
+  res.send(
+    quotes.filter((quote) =>
+      quote.quote.toLowerCase().includes(search.toLowerCase())
+    )
+  );
+});
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(5000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
