32e8df6ee35cf0c78ec65098958d84d5560adfef
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2021-12-15 22:48:39 +0000

    end of level 2
    completed all tasks until the end of level 2

--- server.js
+++ server.js
@@ -13,7 +13,26 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  console.log(quotes);
+  response.send("Tom's Quote Server!  Ask me for /quotes/random, or /quotes");
+});
+app.get("/quotes", function (request, response) {
+  response.send(quotes);
+});
+app.get("/quotes/random", function (request, response) {
+  response.send(pickFromArray(quotes));
+});
+app.get("/quotes/search", function (request, response) {
+  const quotesInSearch = quotes.filter((x) => {
+    return (
+      x.author.toLowerCase().includes(request.query.term.toLowerCase()) ||
+      x.quote.toLowerCase().includes(request.query.term.toLowerCase())
+    );
+  });
+  // if you just want one random one:
+  // response.send(pickFromArray(quotesInSearch));
+  // if you want the list:
+  response.send(quotesInSearch);
 });
 
 //START OF YOUR CODE...
@@ -29,6 +48,7 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+// altered this because it was becoming a pain to keep changing the address
+const listener = app.listen(8000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
