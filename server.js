ca20efb43effb4aef3c91e03d6efdb6bd68e8694
Sinead <77743497+Sinead-CYF@users.noreply.github.com>
2022-01-11 22:09:49 +0000

    Level 1 complete

--- server.js
+++ server.js
@@ -1,34 +1,35 @@
-// server.js
-// This is where your node app starts
-
-//load the 'express' module which makes writing webservers easy
+//load the 'express' module 
 const express = require("express");
 const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+
+//Import ability to host static folders. 
+app.use(express.static(path.join(__dirname, "public")));
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
 
-// Now register handlers for some routes:
-//   /                  - Return some helpful welcome info (text)
-//   /quotes            - Should return all quotes (json)
-//   /quotes/random     - Should return ONE quote (json)
+//Register handlers for routes:
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("Sinead's Quote Server!  Ask me for random quotes using: /quotes/random, or all quotes by using: /quotes");
 });
 
-//START OF YOUR CODE...
+app.get("/quotes", function (request, response) {
+  response.send(quotes);
+});
 
-//...END OF YOUR CODE
+app.get("/quotes/random", function (request, response) {
+  response.send(pickFromArray(quotes));
+});
 
-//You can use this function to pick one element at random from a given array
-//example: pickFromArray([1,2,3,4]), or
-//example: pickFromArray(myContactsArray)
-//
+//random quote selector 
 function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-//Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+//server listens for HTTP requests.
+const listener = app.listen(PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
