de59d61eeadcecca014c5fc7442b8c4f0323110e
Sinead <77743497+Sinead-CYF@users.noreply.github.com>
2022-01-11 22:46:40 +0000

    Intermediate - Echo Complete

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 
-//Import ability to host static folders. 
-app.use(express.static(path.join(__dirname, "public")));
+//server listens for HTTP requests.
+const listener = app.listen(PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
+
+//Import ability to host static folders. 
+app.use(express.static(path.join(__dirname, "public")));
 
 //Register handlers for routes:
 app.get("/", function (request, response) {
@@ -28,8 +33,24 @@ function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-//server listens for HTTP requests.
-const listener = app.listen(PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+//query.param = term: /quotes/search?term=life
+app.get("/quotes/search", (req, res) => {
+  res.json(
+    quotes.filter((arr) => {
+      return (
+        arr.quote.toLowerCase().includes(req.query.term.toLowerCase()) ||
+        arr.author.toLowerCase().includes(req.query.term.toLowerCase())
+      );
+    })
+  );
 });
+
+//sending back search term for concepts sake. 
+app.get("/quotes/echo", (req, res) => {
+  res.json(`You said ${req.query.term}`);
+});
+
+
+
+
 
