08df8360c2c6e09539d53cbcc495e0c09a61c6d5
teniolao <Tennybetty100@gmail.com>
2022-04-24 19:37:12 +0100

    create a server
    create a server (using Node and Express) which serves quotes, in JSON form.

--- server.js
+++ server.js
@@ -13,10 +13,17 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("<h1>Hello there! welcome to my quote server Api</h1>");
 });
 
 //START OF YOUR CODE...
+app.get("/quotes", function (request, response) {
+  response.send(quotes);
+});
+
+app.get("/quotes/random", function (request, response) {
+  response.send(pickFromArray(quotes));
+});
 
 //...END OF YOUR CODE
 
@@ -29,6 +36,8 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+//process.env.PORT
