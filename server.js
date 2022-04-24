55cfcc45da3a90620b90456f8e40dc3e4f8027fb
Alirezabg <91377914+Alirezabg@users.noreply.github.com>
2022-04-24 19:48:48 +0100

    some of level2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
-
+const PORT = 3000;
 //load the quotes JSON
 const quotes = require("./quotes.json");
 
@@ -13,12 +13,35 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send(
+    "Alireza's Quote Server!  Ask me for /quotes/random, or /quotes  You can search by /quotes/search?term=YourWordHere  or you can choose a quote by id /quotes/id/12 "
+  );
 });
 
 //START OF YOUR CODE...
-app.get(`/one`, (req, res) => {
+app.get(`/quotes/random`, (req, res) => {
   res.send(pickFromArray(quotes));
+});
+app.get(`/two`, (req, res) => {
+  res.send([pickFromArray(quotes), pickFromArray(quotes)]);
+});
+app.get(`/quotes`, (req, res) => {
+  res.send(quotes);
+});
+app.get(`/quotes/id/:id`, (req, res) => {
+  const quote = quotes[Number(req.params.id)];
+  if (quote) res.send(quote);
+  res.status(404).send("Whoops, not here sorry!!!");
+});
+app.get("/quotes/search", function (req, res) {
+  const searchQuery = req.query.term.toLowerCase();
+  const filterQuote = quotes.filter(
+    (u) =>
+      u.quote.toLowerCase().includes(searchQuery) ||
+      u.author.toLowerCase().includes(searchQuery)
+  );
+  if (filterQuote) res.send(filterQuote);
+  res.status(404).send("Whoops, not found!!!");
 });
 //...END OF YOUR CODE
 
@@ -31,6 +54,8 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(PORT, function () {
+  // process.env.PORT
   console.log("Your app is listening on port " + listener.address().port);
+  console.log(`My server is running on ${PORT}`);
 });
