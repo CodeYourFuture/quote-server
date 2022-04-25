3be23f7daafa02a3526e00a4a6388fcef89dd711
Timea <timea.reich@gmail.com>
2022-04-25 23:29:25 +0100

    level 1

--- server.js
+++ server.js
@@ -2,9 +2,13 @@
 // This is where your node app starts
 
 //load the 'express' module which makes writing webservers easy
+
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());
 //load the quotes JSON
 const quotes = require("./quotes.json");
 
@@ -12,12 +16,19 @@ const quotes = require("./quotes.json");
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
-app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
-});
 
 //START OF YOUR CODE...
+app.get("/", (req, res) => {
+  res.send("Timi's quote server! Ask me for /quotes/random, or /quotes");
+});
 
+app.get("/quotes", (req, res) => {
+  res.send(quotes);
+});
+
+app.get("/quotes/random", (req, res) => {
+  res.send(pickFromArray(quotes));
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
