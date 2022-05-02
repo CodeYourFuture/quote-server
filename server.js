819a6e2843c295498aab6d96b577f48148cc1948
MatildaAko <matildaako@gmail.com>
2022-05-02 14:20:17 +0100

    Added endpoints for the api

--- server.js
+++ server.js
@@ -13,11 +13,16 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("Matilda's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
 //START OF YOUR CODE...
-
+app.get("/quotes", (request, response) => {
+  response.send(quotes);
+});
+app.get("/quotes/random", (request, response) => {
+  response.send(pickFromArray(quotes));
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
