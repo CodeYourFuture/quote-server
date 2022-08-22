55e7c9cb75e79b94c988cc25561e9420f8fe383a
Baz Murphy <bazmurphy@gmail.com>
2022-08-22 15:23:48 +0100

    challenge intermediate

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const lodash = require("lodash");
 const PORT = 3000;
 
 //load the quotes JSON
@@ -15,22 +16,23 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Baz's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("Baz's Quote Server! Ask me for /quotes/ or /quotes/random or /quotes/search?term=_____");
 });
 
 //START OF YOUR CODE...
 app.get("/quotes", function (request, response) {
-  response.sendFile(__dirname + "/quotes.json")
+  response.sendFile(__dirname + "/quotes.json");
 });
 
 app.get("/quotes/random", function (request, response) {
-  response.send(pickFromArray(quotes)) 
+  // response.send(pickFromArray(quotes));
+  response.send(lodash.sample(quotes));
 });
 
-app.get("/quotes/search", function (req, res) {
-  let searchQuery = req.query.term;
+app.get("/quotes/search", function (request, response) {
+  let searchQuery = request.query.term;
   let searchResults = quotes.filter(element => element.quote.toLowerCase().includes(searchQuery.toLowerCase()) || element.author.toLowerCase().includes(searchQuery.toLowerCase()));
-  res.send(searchResults);
+  response.send(searchResults);
 })
 //...END OF YOUR CODE
 
@@ -38,9 +40,9 @@ app.get("/quotes/search", function (req, res) {
 //example: pickFromArray([1,2,3,4]), or
 //example: pickFromArray(myContactsArray)
 //
-function pickFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
+// function pickFromArray(arr) {
+//   return arr[Math.floor(Math.random() * arr.length)];
+// }
 
 //Start our server so that it listens for HTTP requests!
 const listener = app.listen(PORT || process.env.PORT, function () {
