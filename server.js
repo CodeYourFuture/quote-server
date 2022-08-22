84e60a4b7fdd552ae49b3d5b6830e9ff834af6ad
Baz Murphy <bazmurphy@gmail.com>
2022-08-22 15:01:50 +0100

    level 1 complete

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
+const fs = require("fs");
+const PORT = 3000;
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
@@ -13,11 +15,17 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("Baz's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
 //START OF YOUR CODE...
+app.get("/quotes", function (request, response) {
+  response.sendFile(__dirname + "/quotes.json")
+});
 
+app.get("/quotes/random", function (request, response) {
+  response.send(pickFromArray(quotes)) 
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
@@ -29,6 +37,6 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(PORT || process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
