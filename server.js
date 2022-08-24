97fe958f19120784eb25ca2c600d77e92dac1e95
Baz Murphy <bazmurphy@gmail.com>
2022-08-24 09:28:43 +0100

    refactoring

--- server.js
+++ server.js
@@ -1,46 +1,41 @@
-// server.js
-// This is where your node app starts
-
-//load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
 const lodash = require("lodash");
 const PORT = 3000;
 
-//load the quotes JSON
 const quotes = require("./quotes.json");
 
-// neccessary to avoid CORS errors on Heroku
 const cors = require('cors');
 app.use(cors());
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static('public'));
 
-// Now register handlers for some routes:
-//   /                  - Return some helpful welcome info (text)
-//   /quotes            - Should return all quotes (json)
-//   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
+  // console.log("GET / route");
   // response.send("Baz's Quote Server! Ask me for /quotes/ or /quotes/random or /quotes/search?term=_____");
   response.sendFile(__dirname + "/index.html");
 });
 
-//START OF YOUR CODE...
 app.get("/quotes", function (request, response) {
+  // console.log("GET /quotes route");
   response.sendFile(__dirname + "/quotes.json");
 });
 
 app.get("/quotes/random", function (request, response) {
+  // console.log("GET /quotes/random route");
   // response.send(pickFromArray(quotes));
   response.send(lodash.sample(quotes));
 });
 
 app.get("/quotes/search", function (request, response) {
-  let searchQuery = request.query.term;
-  let searchResults = quotes.filter(element => element.quote.toLowerCase().includes(searchQuery.toLowerCase()) || element.author.toLowerCase().includes(searchQuery.toLowerCase()));
+  // console.log("GET /quotes/search route");
+  const searchQuery = request.query.term;
+  const searchResults = quotes.filter(element => element.quote.toLowerCase().includes(searchQuery.toLowerCase()) || element.author.toLowerCase().includes(searchQuery.toLowerCase()));
   response.send(searchResults);
 })
-//...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
 //example: pickFromArray([1,2,3,4]), or
@@ -50,7 +45,6 @@ app.get("/quotes/search", function (request, response) {
 //   return arr[Math.floor(Math.random() * arr.length)];
 // }
 
-//Start our server so that it listens for HTTP requests!
-app.listen(process.env.PORT || PORT, function () {
-  console.log(`The server is running on ${PORT}`);
+const listener = app.listen(PORT || process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
 });
