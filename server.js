fc075164c49b35fcab789996bd2ba33138a9e8f3
shafiekdavids <shafiekd@gmail.com>
2022-11-06 21:30:06 +0200

    Level 1
    Completed

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
+const arr = require("./quotes.json");
 
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
@@ -18,17 +19,25 @@ app.get("/", function (request, response) {
 
 //START OF YOUR CODE...
 
+app.get("/quotes", function (req, res) {
+  res.send(quotes);
+});
+
+app.get("/quotes/random", function (req, res) {
+  res.send(pickFromArray(arr));
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
 //example: pickFromArray([1,2,3,4]), or
 //example: pickFromArray(myContactsArray)
 //
+
 function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
