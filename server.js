40572f8283a3b4b9fac456eca2578ffdfd992b11
TresorBipa <tresorbipa3@gmail.com>
2022-11-08 11:01:17 +0200

    Challenge 1 Completed

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
-// server.js
 // This is where your node app starts
 
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
+// const { response } = require("express");
+// const { request } = require("http");
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
 
-// Now register handlers for some routes:
-//   /                  - Return some helpful welcome info (text)
-//   /quotes            - Should return all quotes (json)
-//   /quotes/random     - Should return ONE quote (json)
+// Setting up the route Homepage
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.json("Tresor's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
-//START OF YOUR CODE...
+//START OF MY CODE...
 
-//...END OF YOUR CODE
+// Returning All the quotes as JSON
+app.get("/quotes", (request, response) => {
+  response.json(quotes);
+})
+
+// Returning one of the Quotes randomly as JSON
+app.get("/quotes/random",(request,response)=>{
+  response.json(pickFromArray(quotes));
+})
+
+//...END OF MY CODE
 
 //You can use this function to pick one element at random from a given array
 //example: pickFromArray([1,2,3,4]), or
@@ -27,8 +35,7 @@ app.get("/", function (request, response) {
 function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
-
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
