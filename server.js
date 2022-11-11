54b8603bb497676e972fd298a7f4c12550a4219b
BereniceOgundele <102852581+BereniceOgundele@users.noreply.github.com>
2022-11-11 21:13:15 +0200

    Made a few changes
    First commit after changes made. Seeing error when trying to run code using Thunder Client. Need to figure out why error occurs by "googling" it. Then need to come back and test it again

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // This is where your node app starts
 
 //load the 'express' module which makes writing webservers easy
+const { json } = require("express");
 const express = require("express");
 const app = express();
 
@@ -12,11 +13,20 @@ const quotes = require("./quotes.json");
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
-app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
-});
+// app.get("/", function (request, response) {
+//   response.send("Berenice's Quote Server!!!  Ask me for /quotes/random, or /quotes");
+// });
 
 //START OF YOUR CODE...
+app.get("/json", (request, response) => {
+  response.send("Display All Quotes!!");
+});
+console.log(json);
+
+app.listen(3000, () =>
+console.log("Server is listening for requests on port 3000"));
+;
+
 
 //...END OF YOUR CODE
 
@@ -30,5 +40,5 @@ function pickFromArray(arr) {
 
 //Start our server so that it listens for HTTP requests!
 const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log("Your app is listening on port" + listener.address().port);
 });
