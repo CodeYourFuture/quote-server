6ce79bd84e5014eb31d77fdb61ed676fc975bdf7
JanefrancessC <janefrancesschioma@gmail.com>
2022-12-12 17:24:06 +0000

    level 1 done

--- server.js
+++ server.js
@@ -17,7 +17,15 @@ app.get("/", function (request, response) {
 });
 
 //START OF YOUR CODE...
+app.get("/quotes", (req, res) => {
+  // let data = quotes;
 
+  res.send(quotes);
+});
+
+app.get("/quotes/random", (req, res) => {
+  res.send(pickFromArray(quotes));
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
@@ -29,6 +37,10 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+// const listener = app.listen(process.env.PORT, function () {
+//   console.log("Your app is listening on port " + listener.address().port);
+// });
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
 });
