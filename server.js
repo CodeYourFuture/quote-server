990658c67b67daa6eac56176fd509d682bba60ce
AzinYad <103781572+AzinYad@users.noreply.github.com>
2022-12-12 19:50:36 +0000

    quotes route added

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const quotes = require("./quotes.json");
 
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
+app.get("/quotes", (req, res) => res.json(quotes));
+
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
