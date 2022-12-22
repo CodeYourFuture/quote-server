f7fa44d28b7f10bce2426b79db144dbffbd571cc
Mathias02 <matthiasm2016@gmail.com>
2022-12-22 15:37:02 +0200

    initial commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
@@ -17,18 +18,24 @@ app.get("/", function (request, response) {
 });
 
 //START OF YOUR CODE...
+app.get("/quotes", function (request, response) {
+  response.status(200).json(quotes);
+});
 
+app.get("/quotes/random", function (request, response) {
+const {random} = pickFromArray(quotes);
+  response.status(200).json(random);
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
 //example: pickFromArray([1,2,3,4]), or
 //example: pickFromArray(myContactsArray)
-//
 function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+ app.listen(PORT, function () {
+  console.log(`Your app is listening on ${PORT}`);
 });
