47ebfa4b18f627445f19f6479e792ac1a3e80092
hanakisu <105117072+hanakisu@users.noreply.github.com>
2022-12-29 17:35:28 +0000

    done

--- server.js
+++ server.js
@@ -4,52 +4,60 @@
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
+const lodash = require("lodash");
+const cors = require("cors")
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
-
+app.use(cors());
+app.use(express.json());
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("seble's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
 app.get("/quotes", function (request, response) {
-  response.send(quotes);
+  response.json(quotes);
 });
 
-app.get("/quotes/search", function (request, response) {
-  let quotesCopy = quotes;
+app.get("/quotes/search", function (req, response) {
+  let quotesCopy = req.query.term;
+  console.log(quotesCopy)
 
-  if (request.query.term) {
-    quotesCopy = quotesCopy.filter((c) => {
-      return (
-        c.quote.toLowerCase().includes(request.query.term.toLowerCase()) ||
-        c.author.toLowerCase().includes(request.query.term.toLowerCase())
-      );
-    });
-  }
-  response.send(quotesCopy);
+  const filteredQuotes = quotes.filter(
+    (quote) =>
+      quote.quote.toLowerCase().includes(quotesCopy.toLowerCase()) ||
+      quote.author.toLowerCase().includes(quotesCopy.toLowerCase())
+      
+  );
+  response.send(filteredQuotes);
 });
 
-app.get("/random_quote", function (request, response) {
-  let selected = pickFromArray(quotes);
-  response.send(selected);
+app.get("/quote/random", function (request, response) {
+   let selected = pickFromArray(quotes);
+  response.json(selected)
+   
+  // response.send(lodash.sample(quotes));
 });
 
 //START OF YOUR CODE...
-
+app.get("/quotes/random", (req, res) => {
+  let selected = pickFromArray(quotes)
+  res.json(selected)
+   .sendFile(path.join(__dirname, "./", "views", "random.html"));
+})
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
 //example: pickFromArray([1,2,3,4]), or
 //example: pickFromArray(myContactsArray)
 //
-function pickFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
+ function pickFromArray(arr) {
+   return arr[Math.floor(Math.random() * arr.length)];
+ }
 
 //Start our server so that it listens for HTTP requests!
 const listener = app.listen(3000, function () {
