d913ceb64358d70522b45c3cadb1aa030a4b0ea4
Ekrem <ekremteke.uk@gmail.com>
2023-01-06 19:38:29 +0000

    level one was completed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = 3000 || process.env.PORT;
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
@@ -40,5 +40,6 @@ function pickFromArray(arr) {
 
 //Start our server so that it listens for HTTP requests!
 const listener = app.listen(port, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log("Your app is listening on port " + port);
 });
+//listener.address().
