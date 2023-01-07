74d665308c584c62070c70fc3b0e17f8f3ae124b
Adiba Belayete <adiba.du.bd@gmail.com>
2023-01-07 15:23:22 +0000

    complete challenge level 2

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // This is where your node app starts
 
 //load the 'express' module which makes writing webservers easy
+const { response } = require("express");
 const express = require("express");
 const app = express();
 
@@ -13,18 +14,17 @@ const Quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 
-
 //START OF YOUR CODE...
 app.get("/quotes", function (request, response) {
   response.send(Quotes);
 });
 
-const randomQuote = pickFromArray(Quotes)
+const randomQuote = pickFromArray(Quotes);
 app.get("/quotes/random", function (request, response) {
   response.send(randomQuote);
 });
 
-//...END OF YOUR CODE
+
 
 //You can use this function to pick one element at random from a given array
 //example: pickFromArray([1,2,3,4]), or
@@ -34,7 +34,25 @@ function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
+app.get("/quotes/search", (request, response) => {
+  const term = request.query.term.toLowerCase();
+  const filterItem = Quotes.filter(
+    (eachquote) =>
+      eachquote.quote.toLowerCase().includes(term) ||
+      eachquote.author.toLowerCase().includes(term)
+  );
+  response.send(filterItem);
+});
 
+
+
+app.get("/echo", (request, response)=> {
+  const word = request.query.word
+  response.send(`You said ${word}`);
+})
+
+
+//...END OF YOUR CODE
 
 //Start our server so that it listens for HTTP requests!
 const listener = app.listen(process.env.PORT, function () {
