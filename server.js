bd7a17d6fa227e97022f1c46eae840f997835416
youweb3 <105981004+youweb3@users.noreply.github.com>
2023-01-07 14:18:02 +0000

    Completed Task

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // This is where your node app starts
 
 //load the 'express' module which makes writing webservers easy
+const { response } = require("express");
 const express = require("express");
 const app = express();
 
@@ -13,7 +14,15 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("welcome to my web page node");
+});
+
+app.get("/quotes",function (request, response){
+  response.json(quotes);
+});
+
+app.get("/quotes/random",function (request, response){
+  response.json (pickFromArray(quotes));
 });
 
 //START OF YOUR CODE...
