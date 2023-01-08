348d65a29025c001b3b04d8fdb0494b54f1e4bfd
youweb3 <105981004+youweb3@users.noreply.github.com>
2023-01-08 21:02:37 +0000

    Quote Server fix

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
+const port= process.env.PORT || 9090;
 //Start our server so that it listens for HTTP requests!
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
