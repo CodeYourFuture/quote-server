4616808d236624db95ad31b5c41037b641135a91
Miguel-Cabral <miguelanjo2@hotmail.com>
2023-01-09 11:38:49 +0000

    server.js level 1

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // server.js
 // This is where your node app starts
-
+const { response} = require("express");
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
+
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
@@ -14,9 +15,19 @@ const quotes = require("./quotes.json");
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
   response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+
 });
 
+
 //START OF YOUR CODE...
+app.get("/quotes", function (request, response) {
+
+  response.send({quotes});
+});
+app.get("/quotes/random", function (request, response) {
+  const randomQuote = pickFromArray(quotes);
+  response.send({randomQuote});
+});
 
 //...END OF YOUR CODE
 
@@ -29,6 +40,8 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+//process.env.PORT
+const port = 9090;
+const listener = app.listen(port, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
