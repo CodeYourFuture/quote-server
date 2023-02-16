8336733725fb8581e3b3137700347ff13b6a25e8
Marryggk <99277162+Meronggk@users.noreply.github.com>
2023-02-16 13:19:35 +0000

    level 1 final update

--- server.js
+++ server.js
@@ -20,6 +20,12 @@ app.get("/", function (request, response) {
 app.get("/quotes", function (request, response) {
   response.send(quotes);
 })
+
+
+app.get("/quotes/random", function (request, response) {
+  response.send(pickFromArray(quotes));
+})
+
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
