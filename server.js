05aa3350fe5c8e5db662c042c024c0b62a8f86a6
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-03-13 22:51:40 +0000

    Level 2 Done!

--- server.js
+++ server.js
@@ -24,7 +24,17 @@ app.get("/quotes", function (request, response) {
 app.get("/quotes/random", function (request, response) {
   response.send(pickFromArray(quotes));
 });
-
+//level 2, search by term:
+app.get("/quotes/search", (req, res) => {
+  const searchedTerm = req.query.term.toLowerCase();
+  res.send(
+    quotes.filter(
+      (quote) =>
+        quote.quote.toLowerCase().includes(searchedTerm) ||
+        quote.author.toLowerCase().includes(searchedTerm)
+    )
+  );
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
