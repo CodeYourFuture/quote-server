ee09da16353689dca88d63d59cfbef11da249dcb
mickeyhaile2 <108958468+mickeyhaile2@users.noreply.github.com>
2023-03-24 00:13:42 +0000

    level 1 & 2

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
+const Quotes = require("./quotes.json");
 
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
@@ -17,7 +18,25 @@ app.get("/", function (request, response) {
 });
 
 //START OF YOUR CODE...
+app.get("/quotes", function (request, response) {
+  console.log(request.query.quote);
+  response.send({ Quotes });
+});
 
+app.get("/quotes/random", function (request, response){
+  response.send(pickFromArray(Quotes));
+})
+
+app.get("/quotes/search", function (request, response) {
+  console.log(request.query.term);
+  response.send(
+    Quotes.filter(
+      (el) =>
+        el.quote.toLowerCase().includes(request.query.term.toLowerCase()) ||
+        el.author.toLowerCase().includes(request.query.term.toLowerCase())
+    )
+  );
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
