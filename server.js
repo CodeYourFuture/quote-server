66010b78f64e7577a43d1d1af689e55cd1bca190
nataliizab <nataliia.zab@gmail.com>
2023-03-24 18:53:03 +0000

    Added comment for Postman

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.get("/", function (request, response) {
 });
 
 //START OF YOUR CODE...
+//Link for Postman - http://localhost:3000/
 app.listen(3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
