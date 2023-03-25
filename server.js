c6708fb57f67d0a8f2f9ae4e68f8223617a8858d
KhadijaHaji <108950237+KhadijaHaji@users.noreply.github.com>
2023-03-25 11:51:05 +0000

    level 1 completed

--- server.js
+++ server.js
@@ -16,8 +16,20 @@ app.get("/", function (request, response) {
   response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
-//START OF YOUR CODE...
 
+//START OF YOUR CODE...
+app.get("/quotes", function (request, response) {
+  response.send({ quotes });
+});
+
+// app.get("/quotes/random", function (request, response) {
+//   const random = Math.floor(Math.random() * quotes.length);
+//   response.send({ rçandom: quotes[random] });
+// });
+
+app.get("/quotes/random", function (request, response) {
+  response.send({ random : pickFromArray(quotes)});
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
@@ -29,6 +41,6 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(3001, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
