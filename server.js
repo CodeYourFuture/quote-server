c76777c8b04eb8545907d430b69674af9d9dbed1
nataliizab <nataliia.zab@gmail.com>
2023-03-25 12:17:58 +0000

    Added search query to quotes

--- server.js
+++ server.js
@@ -20,18 +20,19 @@ app.get("/", function (request, response) {
 
 //START OF YOUR CODE...
 //Link for Postman - http://localhost:3000/
-app.listen(3000, function () {
-  console.log("Server is listening on port 3000. Ready to accept requests!");
-});
 
 app.get("/quotes", function (request, response) {
-  response.json({quotes});
+  response.json({ quotes });
 });
 
 app.get("/quotes/random", function (request, response) {
   response.json(pickFromArray(quotes));
 });
 
+app.get("/quotes/search", function (request, response) {
+  const term = request.query.term;
+  response.json(searchTerm(term, quotes));
+})
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
@@ -42,7 +43,16 @@ function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
+function searchTerm(string, arr) {
+  const myString = string.toLowerCase();
+  return arr.filter(item => item.quote.toLowerCase().includes(myString) || item.author.toLowerCase().includes(myString))
+}
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(3001, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+
+// /quotes/search?term=life
+// /quotes/search?term=success
+// /quotes/search?term=miss
