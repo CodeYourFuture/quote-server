fed8f3bb6eb95304c49ddcfdefbd5f43b5d65618
laxmikckarki <108900563+laxmikckarki@users.noreply.github.com>
2023-03-25 21:20:36 +0000

    lesson one done

--- server.js
+++ server.js
@@ -13,11 +13,38 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("Laxmi's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
 //START OF YOUR CODE...
 
+app.get("/quotes", function (request, response) {
+  response.send(quotes);
+});
+
+app.get("/quotes/random", function (request, response) {
+  response.send(pickFromArray(quotes));
+});
+
+app.get("/quotes/search", (req, res) => {
+  let searchQuery = req.query.amount;
+  res.send("Hello World! You searched for " + searchQuery);
+});
+
+// app.get("/quotes/search", (req, res) => {
+//   let searchTerm = req.query.term.toLowerCase(); //make the search case-insensitive
+//   let matchingQueries = [];
+//   for (let i = 0; i < quotes.length; i++) {
+//     let quoteText = quotes[i].text.toLowerCase(); //make the search case-insensitive
+//     let authorName = quotes[i].author.toLowerCase(); //make the search case-insensitive
+//     if (quoteText.includes(searchTerm) || authorName.includes(searchTerm)) {
+//       matchingQueries.push(quotes[i]);
+//     }
+//   }
+//   res.status(200).json({
+//     results: matchingQueries,
+//   });
+// });
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
@@ -29,6 +56,6 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(3030, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
