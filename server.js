98db00dad16d474f2a149e7c66f30c38131a1953
Hengameh M.Khani <108895187+HeniMKH@users.noreply.github.com>
2023-03-26 23:39:44 +0100

    level1&2

--- server.js
+++ server.js
@@ -18,6 +18,20 @@ app.get("/", function (request, response) {
 
 //START OF YOUR CODE...
 
+app.get("/quotes", function (request, response) {
+  response.send(quotes);
+});
+
+app.get("/quotes/random", function (request, response) {
+  response.send(pickFromArray(quotes));
+});
+
+app.get("/quotes/search", function (request, response) {
+  const query = request.query.term;
+  const filteredArray = quotes.filter((item) => item["quote"].includes(query));
+  response.send(filteredArray);
+});
+
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
@@ -32,3 +46,5 @@ function pickFromArray(arr) {
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+// npm run dev
