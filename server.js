f8e58865f72b62125d971beb1ab4682deaf14c56
roozbeh <108986775+Roozbehshokri1990@users.noreply.github.com>
2023-03-27 15:45:09 +0100

    leve1.done

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 const express = require("express");
 const app = express();
 
+
 //load the quotes JSON
 const quotes = require("./quotes.json");
 
@@ -13,11 +14,16 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("roozbeh's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
 //START OF YOUR CODE...
-
+app.get("/quotes" , function(request , response) {
+  response.send(quotes)
+});
+app.get("/quotes/random" , function(request , response) {
+  response.send(pickFromArray(quotes))
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
