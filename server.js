41f3b235df2dbd560b9956c3d1a3d46720459b68
Hadi Allahverdi Pournigjeh <108987953+hadipournigjeh@users.noreply.github.com>
2023-03-30 20:58:19 +0100

    I made quotes & random

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
+const lodash = require("lodash")
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
@@ -17,16 +18,22 @@ app.get("/", function (request, response) {
 });
 
 //START OF YOUR CODE...
+app.get("/quotes",  function(req, res){
+  res.send(quotes)
+})
 
+app.get("/quotes/random", function(req, res){
+  res.send(lodash.sample(quotes))
+})
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
 //example: pickFromArray([1,2,3,4]), or
 //example: pickFromArray(myContactsArray)
 //
-function pickFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
+// function pickFromArray(arr) {
+//   return arr[Math.floor(Math.random() * arr.length)];
+// }
 
 //Start our server so that it listens for HTTP requests!
 const listener = app.listen(process.env.PORT, function () {
