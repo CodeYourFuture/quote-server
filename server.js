1c9a9668d6efa707e37d9194f04132f36a9d373a
tony-dev-wm <ta.devopsuk@gmail.com>
2023-03-30 10:14:07 +0100

    wip

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 const express = require("express");
 const cors =require ("cors");
 const app = express();
+const PORT = process.env.PORT || 3001;
 app.use(cors());
 //load the quotes JSON
 const quotes = require("./quotes.json");
