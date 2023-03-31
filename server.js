f7a4a713773b910fac15d175e2c05570e5252ce9
laxmikckarki <108900563+laxmikckarki@users.noreply.github.com>
2023-03-31 21:51:00 +0100

    done

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 // This is where your node app starts
 
 //load the 'express' module which makes writing webservers easy
+const { response } = require("express");
 const express = require("express");
 const app = express();
+// var cors = require("cors");
+// app.use(cors());
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
@@ -16,46 +19,47 @@ app.get("/", function (request, response) {
   response.send("Laxmi's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
+function pickFromArray(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
 //START OF YOUR CODE...
 
 app.get("/quotes", function (request, response) {
-  response.send(quotes);
+  response.json(quotes);
 });
 
 app.get("/quotes/random", function (request, response) {
-  response.send(pickFromArray(quotes));
+  response.json(pickFromArray(quotes));
 });
 
+//level 2
+
+//Create a search handler for quotes
 app.get("/quotes/search", (req, res) => {
-  let searchQuery = req.query.amount;
-  res.send("Hello World! You searched for " + searchQuery);
+  const term = req.query.term; //Get the search term from the URL query string
+
+  // Query the database for quotes containing the specified term
+  const quotes = db.quotes.filter(
+    (quote) =>
+      quote.text.indexOf(term) !== -1 || quote.author.indexOf(term) !== -1
+  );
+
+  // Send an array of quotes as a response
+  res.json(quotes);
 });
 
-// app.get("/quotes/search", (req, res) => {
-//   let searchTerm = req.query.term.toLowerCase(); //make the search case-insensitive
-//   let matchingQueries = [];
-//   for (let i = 0; i < quotes.length; i++) {
-//     let quoteText = quotes[i].text.toLowerCase(); //make the search case-insensitive
-//     let authorName = quotes[i].author.toLowerCase(); //make the search case-insensitive
-//     if (quoteText.includes(searchTerm) || authorName.includes(searchTerm)) {
-//       matchingQueries.push(quotes[i]);
-//     }
-//   }
-//   res.status(200).json({
-//     results: matchingQueries,
-//   });
-// });
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
 //example: pickFromArray([1,2,3,4]), or
 //example: pickFromArray(myContactsArray)
 //
-function pickFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
+// function pickFromArray(arr) {
+//   return arr[Math.floor(Math.random() * arr.length)];
+// }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(3030, function () {
+const listener = app.listen(3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
