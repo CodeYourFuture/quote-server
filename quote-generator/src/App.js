aa21bb3055226628ac627add32d67f10cb2453ca
tony-dev-wm <ta.devopsuk@gmail.com>
2023-03-31 10:23:08 +0100

    react router

--- quote-generator/src/App.js
+++ quote-generator/src/App.js
@@ -1,7 +1,7 @@
 // import {useState, useEffect} from "react";
 import './App.css';
 import Header from './Header';
-
+import {BrowserRouter, Route, Routes, Link} from 'react-router-dom';
 
 
 
@@ -10,6 +10,19 @@ return (
   <div className='App'>
     <Header />
 
+  <BrowserRouter>
+    <nav className='navigation'>
+      <Link to = '/' className= 'navbar'>
+        Random Quote
+      </Link>
+      <Link to = '/quotes' className= 'navbar'>
+        Quote Selection
+      </Link>
+      <Link to = '/quotes/search' className= 'navbar'>
+        Search Quote
+      </Link>
+    </nav>
+  </BrowserRouter>
   </div>
 )
 
