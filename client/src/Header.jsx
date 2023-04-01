fe20fbe792c14c85699caf97a0d9e26e5f43557d
Andriana <andrianasaffo@gmail.com>
2023-04-01 12:37:17 +0100

    CSS edited

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Header = () => {
   return (
     <header className="App-header">
-      <div className="header">
-        <h1>Andriana's Quote Generator</h1>
-        {/* <img src="../../public/quote-generator.png" alt="logo" /> */}
-      </div>
+      {/* <div className="header">
+        <h1>Andriana's Quote Generator</h1> */}
+      <img src="quote-generator.png" alt="logo" className="header" />
+      {/* </div> */}
     </header>
   );
 };
