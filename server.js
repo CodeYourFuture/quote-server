98abebb39b9992c01d03e408bb7378ff46c3950e
Karla G <k.grajalesluna@gmail.com>
2023-04-15 12:37:38 +0100

    server.js level2 done

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // server.js
 // This is where your node app starts
 
-//load the 'express' module which makes writing webservers easy
+//load the 'express' module which makes writing web servers easy
 const express = require("express");
 const app = express();
+
+
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
@@ -13,11 +15,31 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("Neill's Quote Server!  Ask me for /quotes/random, or quotes");
 });
 
-//START OF YOUR CODE...
 
+function filterSearchTerm (arr, str){
+  return (
+    arr.filter((item) =>
+    item.quote.toLowerCase().includes(str.toLowerCase())
+    || item.author.toLowerCase().includes(str.toLowerCase())) 
+  );
+}
+//START OF YOUR CODE... "this it's only for get requeste"
+
+app.get("/quotes", function (request, response) {
+  response.json(quotes);
+});
+
+app.get("/quotes/random", function (request, response) {
+  response.json(pickFromArray(quotes));
+});
+
+app.get("/quotes/search", function (request, response) {
+  const str = request.query.term;
+  response.json(filterSearchTerm(quotes, str));
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
@@ -28,7 +50,8 @@ function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
+
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
---
+++ test.js
@@ -0,0 +1,7 @@
+const arr = ['test', 'one', 'two']
+
+console.log(arr);
+
+
+const filter = arr.filter((item) => item === "two" );
+console.log(filter);
