be92e28a4299562fcdc9e4cd9f97a9c492d8f79c
Andriana <andrianasaffo@gmail.com>
2023-06-13 15:23:41 +0100

    Code cleaned up

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -3,10 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="App-header">
-      {/* <div className="header">
-        <h1>Andriana's Quote Generator</h1> */}
       <img src="quote-generator.png" alt="logo" className="header" />
-      {/* </div> */}
     </header>
   );
 };
