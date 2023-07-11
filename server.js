433abf3dbd6fb1235a397a3748525a703c43c401
Delnia Alipour <delniaalipur@gmail.com>
2023-07-11 21:15:29 +0100

    level 1-2

--- server.js
+++ server.js
@@ -13,10 +13,28 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send(
+    "Delnia's Quote Server!  Ask me for /quotes/random, or /quotes"
+  );
 });
 
 //START OF YOUR CODE...
+app.get("/quotes", function (request, response) {
+  response.send({ quotes });
+});
+
+app.get("/quotes/search", function (request, response) {
+  let searchTerm = request.query.term;
+  let searchedQuotes = quotes.filter(
+    (quote) =>
+      quote.quote.includes(searchTerm) || quote.author.includes(searchTerm)
+  );
+  response.send(searchedQuotes);
+});
+app.get("/quotes/random", function (request, response) {
+  let randomQuote = pickFromArray(quotes);
+  response.send(randomQuote);
+});
 
 //...END OF YOUR CODE
 
