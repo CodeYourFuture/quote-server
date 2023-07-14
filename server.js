108f2847e3003393683c94e8cddeefcefec53eda
Kristina <kristina.dudnyk20@gmail.com>
2023-07-14 10:08:06 +0100

    configured files

--- server.js
+++ server.js
@@ -1,54 +1,44 @@
-// server.js
-// This is where your node app starts
-// Load the full build.
-var _ = require("lodash");
+// this brings in the environment variables from .env
+require("dotenv").config();
 
-//load the 'express' module which makes writing webservers easy
 const express = require("express");
+
+// this prevents Cross Origin Request errors
+const cors = require("cors");
+
+const loadash = require("lodash");
+
 const app = express();
 
-//load the quotes JSON
-const quotes = require("./quotes.json");
+app.use(cors());
 
-// Now register handlers for some routes:
-//   /                  - Return some helpful welcome info (text)
-//   /quotes            - Should return all quotes (json)
-//   /quotes/random     - Should return ONE quote (json)
-app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+const quotes = require("./quotes-with-id.json");
+
+app.get("/", (req, res) => {
+  response.send(
+    "Kristina's Quote Server!  Ask me for /quotes/random, or /quotes"
+  );
 });
 
-//START OF YOUR CODE...
 app.get("/quotes", (req, res) => {
-  res.send(quotes);
   console.log("/quotes");
+  res.send(quotes);
 });
 
 app.get("/quotes/random", (req, res) => {
-  res.send(_.sample(quotes));
   console.log("/quotes/random");
+  res.send(loadash.sample(quotes));
 });
 
 app.get("/quotes/search", (req, res) => {
+  console.log("/quotes/search");
   let searchValue = req.query.term;
   let filteredArr = quotes.filter((element) =>
     element.quote.toLowerCase().includes(searchValue)
   );
   res.send(filteredArr);
-  console.log("/quotes/search");
 });
-//...END OF YOUR CODE
 
-//You can use this function to pick one element at random from a given array
-//example: pickFromArray([1,2,3,4]), or
-//example: pickFromArray(myContactsArray)
-//
-// function pickFromArray(arr) {
-//   return arr[Math.floor(Math.random() * arr.length)];
-// }
-
-//Start our server so that it listens for HTTP requests!
-app.listen(5500, function () {
-  // console.log("Your app is listening on port " + listener.address().port);
-  console.log("Your app is listening on port ");
+app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + process.env.PORT);
 });
