b696c2d9adeaff93a2c4d621d4806062f55a30c9
saqibjvd <70890519+saqibjvd@users.noreply.github.com>
2023-07-15 12:56:58 +0100

    quote-server
    Level 1: part 1
     Add a `/quotes` route

--- server.js
+++ server.js
@@ -12,12 +12,14 @@ const quotes = require("./quotes.json");
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
-app.get("/", function (request, response) {
+app.get("/", (request, response) => {
   response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
 //START OF YOUR CODE...
-
+app.get("/quotes", (request,response) => {
+  response.json(quotes)
+})
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
@@ -27,8 +29,15 @@ app.get("/", function (request, response) {
 function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
+// app.get("/quotes/random", (request, response) => {
+//   response.send(pickFromArray(quotes));
+// });
+
+// function search() {
+
+// }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
