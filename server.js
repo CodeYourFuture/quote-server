04d82f2a84ef0d56817c14b1ee86cf34e4d7bd46
Saliha Popal <salihapopal@gmail.com>
2023-07-17 16:34:14 +0100

    Level 1, 2

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 const express = require("express");
 const app = express();
 
+
 //load the quotes JSON
 const quotes = require("./quotes.json");
 
@@ -13,10 +14,49 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("Saliha's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
 //START OF YOUR CODE...
+
+app.get("/quotes", function (request, response) {
+  response.send({quotes});
+});
+
+app.get("/quotes/random", function (request, response) {
+  
+   // Pick a random quote from the array using the pickFromArray function
+   const randomQuote = pickFromArray(quotes);
+ 
+   // Send the random quote as the response
+   response.send(randomQuote);
+});
+
+//  http://localhost:62297/quotes/search?term=life/success/miss
+// app.get("/quotes/search", function (request, response) {
+//   // let term = request.query.term
+//   // response.send(term);
+// });
+
+// advance
+app.get("/quotes/search", function (request, response) {
+  const term = request.query.term;
+  const searchResults = searchQuotes(term);
+  response.send({searchResults});
+});
+
+
+// ## Level 2 Challenge - allow quote _searches_!
+function searchQuotes(term) {
+  const results = quotes.filter(
+    (quote) =>
+      quote.quote.toLowerCase().includes(term.toLowerCase()) ||
+      quote.author.toLowerCase().includes(term.toLowerCase()) ||
+      quote === ""
+  );
+  return results;
+}
+
 
 //...END OF YOUR CODE
 
@@ -32,3 +72,5 @@ function pickFromArray(arr) {
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+
