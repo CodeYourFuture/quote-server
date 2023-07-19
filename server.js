f9d7a3c681b3ef6b14af2c65be998f611b225a3e
AndriusIsin <isin.andrius@gmail.com>
2023-07-19 19:55:30 +0100

    done

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
-
+// const port = 9090;
 //load the quotes JSON
 const quotes = require("./quotes.json");
 
@@ -13,10 +13,26 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send(
+    "Andrius's Quote Server!  Ask me for /quotes/random, or /quotes"
+  );
 });
 
 //START OF YOUR CODE...
+app.get("/quotes", function (request, response) {
+  response.send({ quotes });
+});
+app.get("/quotes/random", function (request, response) {
+  response.send(pickFromArray(quotes));
+});
+app.get("/quotes/search", function (request, response) {
+  const query = request.query.term;
+  response.send(
+    quotes.filter((quote) =>
+      quote.quote.toLowerCase().includes(query.toLowerCase())
+    )
+  );
+});
 
 //...END OF YOUR CODE
 
@@ -32,3 +48,6 @@ function pickFromArray(arr) {
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
+// const listener = app.listen(port, function () {
+//   console.log("Your app is listening on port ");
+// });
