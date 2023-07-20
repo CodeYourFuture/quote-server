a582fc5ec701e34c84a12adc150d61fd17ffb627
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-07-20 23:33:53 +0100

    added react-frontend

---
+++ FrontEnd/quotes/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import QuotesCard from "./QuotesCard.js"
+
+function App() {
+  return (
+    <div className="App">
+      <QuotesCard />
+      
+    </div>
+  );
+}
+
+export default App;
