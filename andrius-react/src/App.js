292f703632a3648820d232416bb73df82a56f21a
AndriusIsin <isin.andrius@gmail.com>
2023-07-29 09:30:27 +0100

    added css

--- andrius-react/src/App.js
+++ andrius-react/src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import RandomQuote from "./RandomQuote";
 function App() {
   return (
-    <div className="app">
-      <h1>Star Wars quotes</h1>
-      <RandomQuote />;
-    </div>
+    <body className="app">
+      <div className="container">
+        <h1>Star Wars quotes</h1>
+        <RandomQuote />;
+      </div>
+    </body>
   );
 }
 export default App;
