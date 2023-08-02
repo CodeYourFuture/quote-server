77508e201f8cac22b84514b08e241a9297bf9334
Saliha Popal <salihapopal@gmail.com>
2023-08-02 10:10:21 +0100

    Added lodash to the file

--- server.js
+++ server.js
@@ -1,94 +1,9 @@
-// // server.js
-// // This is where your node app starts
-
-// //load the 'express' module which makes writing webservers easy
-// const express = require("express");
-// const cors = require('cors');
-
-// const app = express();
-// app.use(cors());
-
-// const quotes = require("./quotes.json");
-
-// // Now register handlers for some routes:
-// //   /                  - Return some helpful welcome info (text)
-// //   /quotes            - Should return all quotes (json)
-// //   /quotes/random     - Should return ONE quote (json)
-// app.get("/", function (request, response) {
-//   response.send(" Quote Server!  Ask me for /quotes/random, or /quotes");
-// });
-
-// app.get("/quotes", function (request, response) {
-//   response.send({quotes});
-// });
-
-// app.get("/quotes/random", function (request, response) {
-//    // Pick a random quote from the array using the pickFromArray function
-//    const randomQuote = pickFromArray(quotes);
-//    // Send the random quote as the response
-//    response.send(randomQuote);
-// });
-
-//  // Route for searching by term
-//  // - `/quotes/search?term=life`
-// app.get("/quotes/search", function (request, response) {
-//   let term = request.query.term
-//   response.send(term);
-// });
-
-// // ## Level 2 Challenge - allow quote _searches_!
-// function searchQuotes(term) {
-//   const results = quotes.filter(
-//     (quote) =>
-//       quote.quote.toLowerCase().includes(term.toLowerCase()) ||
-//       quote.author.toLowerCase().includes(term.toLowerCase()) ||
-//       quote === ""
-//   );
-//   return results;
-// }
-
-// // advance
-// app.get("/quotes/search", function (request, response) {
-//   const term = request.query.term;
-//   const searchResults = searchQuotes(term);
-//   response.send({searchResults});
-// });
-
-// //You can use this function to pick one element at random from a given array
-// //example: pickFromArray([1,2,3,4]), or
-// //example: pickFromArray(myContactsArray)
-// //
-// function pickFromArray(arr) {
-//   return arr[Math.floor(Math.random() * arr.length)];
-// }
-
-// // // // Route search for  `/echo?word=ismail
-// // // app.get("/quotes/search", function(request, response){
-// // //   console.log(request.query.word, "<----search word");
-// // //   const word = quotes.filter((quote) => quote.word === word.toLowerCase()
-// // //   );
-// // //   return word;
-// // // })
-
-
-// //Start our server so that it listens for HTTP requests!
-// const listener = app.listen(process.env.PORT, function () {
-//   console.log("Your app is listening on port " + listener.address().port);
-// });
-
-
-
-
-
-
-
-
-
 // server.js
 // This is where your node app starts
 
 //load the 'express' module which makes writing webservers easy
 const { query } = require("express");
+const lodash = require("lodash");
 const cors = ("cors");
 const express = require("express");
 const app = express();
@@ -126,6 +41,11 @@ app.get("/quotes/random", function (request, response) {
   // Send the random quote as the response
   response.send(randomQuote);
 });
+
+app.get('/quotes/random', function(request, response) {
+  const randomQuote = sample(lodash);
+  response.send(randomQuote);
+})
 
 // - `/quotes/search?term=life`
 //  http://localhost:62297/quotes/search?term=life/success/miss
