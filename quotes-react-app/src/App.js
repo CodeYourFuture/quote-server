10b1e9a1e6f3fab02d966605583215b4f859c4bf
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2023-08-05 15:32:30 +0100

    Button done

--- quotes-react-app/src/App.js
+++ quotes-react-app/src/App.js
@@ -1,31 +1,31 @@
 import React,{useState,useEffect} from "react"
 import './App.css';
-import Button from "./components/Button";
+import QuotesButton from "./components/QuotesButton";
 
 
 function App() {
 
-const[quote, setQuote]=useState({ quote: '', author: '' });
+  const[quote, setQuote]=useState({ quote: '', author: '' });
 
-const GenerateQuote = () => {
-  fetch("https://quote-server-fu7w.onrender.com/quotes/random")
-    .then((result) => result.json())
-    .then((data) => {
-      setQuote(data);
-    });
-};
+  const GenerateQuote = () => {
+    fetch("https://quote-server-fu7w.onrender.com/quotes/random")
+      .then((result) => result.json())
+      .then((data) => {
+        setQuote(data);
+      });
+  };
 
-useEffect(()=>{
-  GenerateQuote();
-},[]);
+  useEffect(()=>{
+    GenerateQuote();
+  },[]);
 
-  return (
+  return ( 
     <div className="App">
      <h1>{quote.quote}</h1>
      <h2>{quote.author}</h2>
-     <Button onClick={GenerateQuote}/>
+     <QuotesButton onClick={GenerateQuote}/>
     </div>
-  );
+  )
 }
 
 export default App;
--- quotes-react-app/src/components/Button.js
+++
@@ -1,7 +0,0 @@
-
-function Button({onClick}){
-    return(
-    <Button onClick={onClick}>Generate New Quote</Button>
-    );
-}
-export default Button;
