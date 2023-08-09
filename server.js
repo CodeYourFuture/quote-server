52158e7e2462a5eb4ec924aac0f557120634fd3a
AlessandraSousa <violetanomar@gmail.com>
2023-08-09 12:24:54 +0100

    Server 1

--- server.js
+++ server.js
@@ -13,10 +13,20 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("Course's Quote Server!  Ask me for /quotes/random, or have a look at all quotes");
 });
 
 //START OF YOUR CODE...
+
+app.get("/quotes", (req, res) => {
+  // let data = quotes;
+
+  res.send(quotes);
+});
+
+app.get("/quotes/random", (req, res) => {
+  res.send(pickFromArray(quotes));
+});
 
 //...END OF YOUR CODE
 
@@ -29,6 +39,13 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+
+// code
+
+// const listener = app.listen(process.env.PORT, function () {
+//   console.log("Your app is listening on port " + listener.address().port);
+// });
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
 });
